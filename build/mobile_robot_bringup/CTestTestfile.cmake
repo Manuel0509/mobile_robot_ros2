27a9602671fdfb6738004b087a65018f8ad44d24
Yousif <es-yousif.mostafa24@alexu.edu.eg>
2024-11-05 17:31:10 +0200

    Initial Commit

---
+++ build/mobile_robot_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yousif/mobile_robot_ros2/src/mobile_robot_bringup
+# Build directory: /home/yousif/mobile_robot_ros2/build/mobile_robot_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/mobile_robot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/yousif/mobile_robot_ros2/install/mobile_robot_description/share/mobile_robot_description/package.sh"
